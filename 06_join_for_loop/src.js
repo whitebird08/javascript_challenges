f719bec68a734d9180f6b989d623c577695131f8
Ryan Bird <ryan01bird@gmail.com>
2015-08-05 20:33:01 -0600

    06 has 2 failures on last one

--- 06_join_for_loop/src.js
+++ 06_join_for_loop/src.js
@@ -3,9 +3,56 @@
 */
 
 module.exports = {
-
+  
   joinWithFor: function(list) {
     // your code here
-  }
+    return list.join('');
+  },
+ 
+  joinWithForAndIndex: function(array) {
+    var newArray = [];
 
+    for(var i=0; i < array.length; i++){
+      newArray.push(array[i]);
+      newArray.push((i));
+       }  
+       return newArray.join("")
+  },
+
+ 
+  joinWithoutOddCharacters: function(array){
+    
+    var newArray = [];
+  
+    for (var i=0; i<array.length; i+=2){
+   
+          newArray.push(array[i])   
+    }
+    return newArray.join('')
+  },
+  
+ 
+    joinWithForAndToken: function(array, token){
+      var newArray = [];
+      for(var i=0; i<array.length; i++)
+        newArray.push(array[i])
+      return newArray.join(token);
+    },
+ 
+  joinWithForAndAlternatingTokens: function(array, token1, token2){
+    var newArray = [];
+    var output = [];
+      for (var i=0; i<array.length; i++){
+      newArray.push(array[i])
+      }
+         newArray.join(token1)
+    
+      for(var j=0; j<newArray.length; j++){
+        if(newArray[i]%4==0){newArray[i].replace(token2)
+        }
+       } 
+   output.push(newArray)
+   return output
+  }
+ 
 }
