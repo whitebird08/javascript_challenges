adcab75643ceadbdbbe044fe437b1be639659c6d
Ryan Bird <ryan01bird@gmail.com>
2015-08-05 19:01:09 -0600

    04 has 0 failures

--- 04_objects/src.js
+++ 04_objects/src.js
@@ -1,5 +1,42 @@
 module.exports = {
   person: function() {
     // your code here
+    return {  firstName: 'John',
+              lastName: 'Mayer'
+    } 
+  },
+
+  sweetnessProperty: function(obj){
+    if (obj.sweetness === undefined){
+      return undefined
+    }
+      return obj.sweetness;   
+  },
+
+  savorynessProperty: function(obj){
+    if (obj.sweetness = undefined){
+      return undefined
+    }
+    return obj["savory-ness"];
+  },
+
+  keys: function(obj){
+    return Object.keys(obj)
+  },
+
+  sortedKeys: function(obj){
+    return Object.keys(obj).sort();
+  },
+
+  reverseSortedKeys: function(obj){
+    return Object.keys(obj).sort().reverse();
   }
+
+
+
+
+
+
 }
+
+
